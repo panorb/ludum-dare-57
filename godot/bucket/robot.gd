6d6c9e26dc193c78421d77fe29b014c2e7d60014
Toni Beier <toni.beier@hrz.tu-chemnitz.de>
2025-04-05 14:04:44 +0200

    Add draft of rope code

--- godot/bucket/robot.gd
+++ godot/bucket/robot.gd
@@ -1,4 +1,10 @@
-extends CharacterBody2D
+class_name Robot extends CharacterBody2D
+
+@onready var collision_shape_2d := %CollisionShape2D;
+@onready var rope_ancer : Node2D = %RopeAncer;
+
+var rope_ancer_position: Vector2:
+	get: return rope_ancer.global_position
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_left"):
