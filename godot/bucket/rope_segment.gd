d3da1a6fffa1a4cf5e8ecf3b314284aeb6fc71af
Paul Norberger <hi@paulnorberger.de>
2025-04-05 15:25:29 +0200

    Fix rope creation

--- godot/bucket/rope_segment.gd
+++ godot/bucket/rope_segment.gd
@@ -1,10 +1,6 @@
 class_name RopeSegment extends RigidBody2D
 
-@onready var start : Node2D = %Start
 @onready var end : Node2D = %End
 
-var start_position: Vector2:
-	get: return start.global_position
-	
 var end_position: Vector2:
 	get: return end.global_position
