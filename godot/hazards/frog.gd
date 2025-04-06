4f06b6e286beaf52f74a23f8de518ff8341ff99f
Paul Norberger <hi@paulnorberger.de>
2025-04-06 15:10:01 +0200

    Work on flipping entities & mushrooms

--- godot/hazards/frog.gd
+++ godot/hazards/frog.gd
@@ -1,11 +1,15 @@
+class_name Frog
 extends RigidBody2D
 
 var jumped = false
+@export var flipped : bool = false
 
 func _ready() -> void:
 	$ReadyArea.body_entered.connect(_on_ReadyArea_body_entered)
 	$ReadyArea.body_exited.connect(_on_ReadyArea_body_exited)
 	$JumpArea.body_entered.connect(_on_JumpArea_body_entered)
+	$HitBox.body_entered.connect(_on_HitBox_body_entered)
+	$AnimatedSprite2D.flip_h = flipped
 
 func _on_ReadyArea_body_entered(body: Node2D):
 	if body.name == "Bucket" and not jumped:
@@ -20,14 +24,27 @@ func _on_JumpArea_body_entered(body: Node2D):
 		$AnimatedSprite2D.play("jump")
 		var direction := body.global_position - global_position
 		apply_central_impulse((body.global_position - global_position) * 2.5)
+		collision_mask = 0
 		$AnimatedSprite2D.flip_h = direction.x <= 0
 		
 		jumped = true
 
+func _on_HitBox_body_entered(body: Node2D):
+	if body.name == "Bucket":
+		body.apply_central_impulse((body.global_position - global_position) * 35.0)
+		get_tree().get_first_node_in_group("game").player_life_points -= 1
+
 var falling = false
+var falling_time = 0.0
 
 func _process(delta: float) -> void:
 	print(linear_velocity)
+	if falling:
+		falling_time += delta
+	
+	if falling_time > 6.0:
+		queue_free()
+	
 	if jumped and linear_velocity.y > 200.0 and not falling:
 		falling = true
 		$AnimatedSprite2D.play("fall")
--- godot/shared/entities_post_import.gd
+++ godot/shared/entities_post_import.gd
@@ -1,7 +1,9 @@
 @tool
+extends Node
 
 var bat_scene : PackedScene = preload("res://hazards/bat.tscn")
 var frog_scene : PackedScene = preload("res://hazards/frog.tscn")
+var mushroom_scene : PackedScene = preload("res://hazards/mushroom.tscn")
 
 func post_import(entity_layer: LDTKEntityLayer) -> LDTKEntityLayer:
 	var definition: Dictionary = entity_layer.definition
@@ -13,11 +15,19 @@ func post_import(entity_layer: LDTKEntityLayer) -> LDTKEntityLayer:
 			var bat_root = bat_scene.instantiate()
 			bat_root.name = entity.iid
 			bat_root.position = entity.position
+			bat_root.flipped = entity.fields.flip
 			entity_layer.add_child(bat_root)
 		elif entity.identifier == "Frog":
-			var frog_root = frog_scene.instantiate()
+			print(entity)
+			var frog_root : Frog = frog_scene.instantiate()
 			frog_root.name = entity.iid
 			frog_root.position = entity.position
+			frog_root.flipped = entity.fields.flip
 			entity_layer.add_child(frog_root)
+		elif entity.identifier == "Mushroom":
+			var mushroom_root = mushroom_scene.instantiate()
+			mushroom_root.name = entity.iid
+			mushroom_root.position = entity.position
+			entity_layer.add_child(mushroom_root)
 
 	return entity_layer
